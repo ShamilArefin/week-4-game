76a0b10d6361c8f2ec53ea2fe583119994de80e1
Shamil Arefin <landoflitany@gmail.com>
2018-02-22 20:28:07 -0600

    commited

---
+++ assets/javascript/game.js
@@ -0,0 +1,11 @@
+var randumgem = Math.floor((Math.random() * 12) + 1);
+var peridot;
+var amethyst;
+var garnet;
+var smokey;
+
+$(document).ready(function() {
+    $(document).on(click, function(){
+
+    });
+};
