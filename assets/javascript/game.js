94512be79082e60c71c2c2e2491ba2d641d37f07
Shamil Arefin <landoflitany@gmail.com>
2018-03-03 11:25:18 -0600

    commit

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,11 +1,21 @@
-var randumgem = Math.floor((Math.random() * 12) + 1);
-var peridot;
-var amethyst;
-var garnet;
-var smokey;
+var randumgem1 = Math.floor((Math.random() * 12) + 1);
+var randumgem2 = Math.floor((Math.random() * 12) + 1);
+var randumgem3 = Math.floor((Math.random() * 12) + 1);
+var randumgem4 = Math.floor((Math.random() * 12) + 1);
+var randumgoal = Math.floor((Math.random() * 101) + 19);
+var score = 0;
+console.log("gem " + randumgem1);
+console.log("gem " + randumgem2);
+console.log("gem " + randumgem3);
+console.log("gem " + randumgem4);
+console.log("goal " + randumgoal);
+var peridot = 0;
+var amethyst = 0;
+var garnet = 0;
+var smokey = 0;
 
-$(document).ready(function() {
-    $(document).on(click, function(){
-
+$(document).ready(function(){
+    $(".peridot").on("click", function(){
+        peridot = randumgem1;
     });
-};
+});
